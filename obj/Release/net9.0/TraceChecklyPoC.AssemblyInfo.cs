32c24566a6e3917112c3c7d7efe26573339bb629
Rodrigo Souza <rodrigo.souza@ed.dev.br>
2025-07-24 14:22:29 -0400

    Atualizando exporter para collector.

--- obj/Release/net9.0/TraceChecklyPoC.AssemblyInfo.cs
+++ obj/Release/net9.0/TraceChecklyPoC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraceChecklyPoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c220e148976aacf4fc891045b1fd0d05a005e4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraceChecklyPoC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraceChecklyPoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
